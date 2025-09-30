70f6e8bebc9b92b5fe0b059e851c9d14adfe1121
Nikolaj J-K <kagenogmig@gmail.com>
2025-09-30 17:30:02 +0200

    Can change username

--- src/gui/gui.rs
+++ src/gui/gui.rs
@@ -1,11 +1,29 @@
 use std::error::Error;
-use std::rc::Rc;
-use std::cell::RefCell;
 
 slint::include_modules!();
 
-fn get_username() -> String {
-    let dialog = LoginDialog::new().unwrap();
+fn main() -> Result<(), Box<dyn Error>> {
+    let ui = AppWindow::new()?;
+    let dialog = LoginDialog::new()?;
+
+    ui.on_logout({
+        let dialog = dialog.as_weak().unwrap();
+        move || {
+            println!("Login requested.");
+            dialog.show().unwrap();
+        }
+    });
+    ui.on_request_increase_value({
+        let ui_handle = ui.as_weak();
+        move || {
+            let ui = ui_handle.unwrap();
+            ui.set_counter(ui.get_counter() + 1);
+        }
+    });
+
+    ui.show()?;
+
+
     dialog.on_check_ok({
         let dialog_handle = dialog.as_weak();
         let dialog = dialog_handle.unwrap();
@@ -17,37 +35,28 @@ fn get_username() -> String {
             }
         }
     });
-    
-    let username = Rc::new(RefCell::new(String::new()));
-    let username_clone = username.clone();
+
     dialog.on_login({
-            let dialog = dialog.as_weak().unwrap();
-            move |name, remember| {
-            println!("Login with username: {}{}.", name, if remember { ", which will be remembered" } else { "" });
-            *username_clone.borrow_mut() = name.to_string();
+        let dialog = dialog.as_weak().unwrap();
+        move |name, remember| {
+            println!(
+                "Login with username: {}{}.",
+                name,
+                if remember {
+                    ", which will be remembered"
+                } else {
+                    ""
+                }
+            );
+
             dialog.hide().expect("Failed to hide dialog");
+
+            ui.set_username(name);
         }
     });
-    dialog.run().unwrap();
-    return username.borrow().clone();
-}
+    
+    dialog.show()?;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    let username = get_username();
-    let ui = AppWindow::new()?;
-    ui.set_username(username.into());
-    ui.on_request_login(|| {
-        println!("Login requested from UI");
-    });
-    ui.on_request_increase_value({
-        let ui_handle = ui.as_weak();
-        move || {
-            let ui = ui_handle.unwrap();
-            ui.set_counter(ui.get_counter() + 1);
-        }
-    });
-
-    ui.run()?;
-
+    slint::run_event_loop()?;
     Ok(())
 }
